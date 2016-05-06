660fd34b1add7d5b4e94d5a325c5a8079d1e48dd
makkarpov <me@makkarpov.ru>
2016-05-06 18:43:35 +0300

    Replaced `shouldNot compile` to `typeCheck` in tests,
    ... so parse errors would generate a test failure.

--- scalingua/src/test/scala/ru/makkarpov/scalingua/test/MacroTest.scala
+++ scalingua/src/test/scala/ru/makkarpov/scalingua/test/MacroTest.scala
@@ -31,23 +31,23 @@ class MacroTest extends FlatSpec with Matchers {
     t"Number is $x" shouldBe s"{s:Number is $x}"
     t"Number is $x%(y)" shouldBe s"{s:Number is $x}"
 
-    """ t"Number is ${x+10}" """ shouldNot compile
+    """ t"Number is ${x+10}" """ shouldNot typeCheck
     t"Number is ${x+10}%(y)" shouldBe s"{s:Number is ${x+10}}"
    }
 
   it should "handle singular forms" in {
     t("Hello, world!") shouldBe "{s:Hello, world!}"
 
-    """ t("Test %(x)") """ shouldNot compile
-    """ t("Test", "x" -> 20) """ shouldNot compile
-    """ t("Test %(y)", "x" -> 20) """ shouldNot compile
+    """ t("Test %(x)") """ shouldNot typeCheck
+    """ t("Test", "x" -> 20) """ shouldNot typeCheck
+    """ t("Test %(y)", "x" -> 20) """ shouldNot typeCheck
 
     t("Test %(x)", "x" -> 20) shouldBe "{s:Test 20}"
     t("Test %(x)", ("x", 30)) shouldBe "{s:Test 30}"
     t("Test %(x)%(x)%(x)", "x" -> true) shouldBe "{s:Test truetruetrue}"
 
-    """ val s = "1"; t(s) """ shouldNot compile
-    """ val s = "x"; t("1", s -> 1) """ shouldNot compile
+    """ val s = "1"; t(s) """ shouldNot typeCheck
+    """ val s = "x"; t("1", s -> 1) """ shouldNot typeCheck
 
     { val i = 2; t("1%(1)3", "1" -> i ) } shouldBe "{s:123}"
   }
@@ -55,16 +55,16 @@ class MacroTest extends FlatSpec with Matchers {
   it should "handle contextual singular forms" in {
     tc("test", "Hello, world!") shouldBe "{sc:test:Hello, world!}"
 
-    """ tc("1", "%(x)") """ shouldNot compile
-    """ tc("1", "2", "3" -> 4) """ shouldNot compile
-    """ tc("1", "%(2)", "3" -> 4) """ shouldNot compile
+    """ tc("1", "%(x)") """ shouldNot typeCheck
+    """ tc("1", "2", "3" -> 4) """ shouldNot typeCheck
+    """ tc("1", "%(2)", "3" -> 4) """ shouldNot typeCheck
 
     tc("1", "%(2)", "2" -> 3) shouldBe "{sc:1:3}"
     tc("1", "%(2)%(3)", "2" -> 4, "3" -> 5) shouldBe "{sc:1:45}"
 
-    """ val s = "1"; tc(s, "1") """ shouldNot compile
-    """ val s = "2"; tc("1", s) """ shouldNot compile
-    """ val s = "3"; tc("1", "2", s -> 4) """ shouldNot compile
+    """ val s = "1"; tc(s, "1") """ shouldNot typeCheck
+    """ val s = "2"; tc("1", s) """ shouldNot typeCheck
+    """ val s = "3"; tc("1", "2", s -> 4) """ shouldNot typeCheck
 
     { val i = 3; tc("1", "%(2)", "2" -> i) } shouldBe "{sc:1:3}"
   }
@@ -73,37 +73,37 @@ class MacroTest extends FlatSpec with Matchers {
     p("Hello, %(n) world!", "Hello, %(n) worlds!", 1) shouldBe "{p:Hello, 1 world!:Hello, 1 worlds!:1}"
 
     // %(n) should be present in string
-    """ p("Test", "Tests", 1) """ shouldNot compile
+    """ p("Test", "Tests", 1) """ shouldNot typeCheck
 
     val n = 10 + 20
     p("%(n)", "%(n)s", n) shouldBe "{p:30:30s:30}"
 
-    """ p("1%(n)", "2", 3) """ shouldNot compile
-    """ p("1", "2%(n)", 3) """ shouldNot compile
-    """ p("1%(n)%(x)", "2%(n)", 3) """ shouldNot compile
-    """ p("1%(n)", "2%(n)%(x)", 3) """ shouldNot compile
+    """ p("1%(n)", "2", 3) """ shouldNot typeCheck
+    """ p("1", "2%(n)", 3) """ shouldNot typeCheck
+    """ p("1%(n)%(x)", "2%(n)", 3) """ shouldNot typeCheck
+    """ p("1%(n)", "2%(n)%(x)", 3) """ shouldNot typeCheck
 
     p("%(n)s%(x)", "%(x)p%(n)", 3, "x" -> 2) shouldBe "{p:3s2:2p3:3}"
 
-    """ p("1%(n)%(x)", "2%(n)%(y)", 3, "x" -> 1, "y" -> 2) """ shouldNot compile
-    """ val s = "%(n)"; p(s, "%(n)", 3) """ shouldNot compile
-    """ val s = "%(n)"; p("%(n)", s, 3) """ shouldNot compile
+    """ p("1%(n)%(x)", "2%(n)%(y)", 3, "x" -> 1, "y" -> 2) """ shouldNot typeCheck
+    """ val s = "%(n)"; p(s, "%(n)", 3) """ shouldNot typeCheck
+    """ val s = "%(n)"; p("%(n)", s, 3) """ shouldNot typeCheck
   }
 
   it should "handle contextual plural forms" in {
     pc("1", "2%(n)", "%(n)3", 4) shouldBe "{pc:1:24:43:4}"
 
-    """ pc("0", "1", "2%(n)", 3) """ shouldNot compile
+    """ pc("0", "1", "2%(n)", 3) """ shouldNot typeCheck
 
-    """ pc("0", "1%(n)", "2", 3) """ shouldNot compile
-    """ pc("0", "1", "2%(n)", 3) """ shouldNot compile
-    """ pc("0", "1%(n)%(x)", "2%(n)", 3) """ shouldNot compile
-    """ pc("0", "1%(n)", "2%(n)%(x)", 3) """ shouldNot compile
+    """ pc("0", "1%(n)", "2", 3) """ shouldNot typeCheck
+    """ pc("0", "1", "2%(n)", 3) """ shouldNot typeCheck
+    """ pc("0", "1%(n)%(x)", "2%(n)", 3) """ shouldNot typeCheck
+    """ pc("0", "1%(n)", "2%(n)%(x)", 3) """ shouldNot typeCheck
 
     pc("1", "%(x)%(y)%(n)%(x)", "%(y)%(n)%(x)%(y)", 1, "x" -> 2, "y" -> 3) shouldBe "{pc:1:2312:3123:1}"
 
-    """ val s = "1"; pc(s, "%(n)", "%(n)", 1) """ shouldNot compile
-    """ val s = "%(n)"; pc("1", s, "%(n)", 1) """ shouldNot compile
+    """ val s = "1"; pc(s, "%(n)", "%(n)", 1) """ shouldNot typeCheck
+    """ val s = "%(n)"; pc("1", s, "%(n)", 1) """ shouldNot typeCheck
   }
 
   it should "handle multiline strings" in {
@@ -135,8 +135,8 @@ class MacroTest extends FlatSpec with Matchers {
 
     t("1".stripMargin('1')) shouldBe "{s:}"
 
-    """ val c = '1'; t("1".stripMargin(c)) """ shouldNot compile
-    """ val s = "1"; t(s.stripMargin('1')) """ shouldNot compile
+    """ val c = '1'; t("1".stripMargin(c)) """ shouldNot typeCheck
+    """ val s = "1"; t(s.stripMargin('1')) """ shouldNot typeCheck
   }
 
   it should "handle plural strings" in {
@@ -149,17 +149,17 @@ class MacroTest extends FlatSpec with Matchers {
     p"Msg $n" shouldBe "{p:Msg 10:Msg 10:10}"
 
     // Multiple integer variables, each could be a plural number:
-    """ p"test $n $n" """ shouldNot compile
-    """ p"$k test $n" """ shouldNot compile
+    """ p"test $n $n" """ shouldNot typeCheck
+    """ p"$k test $n" """ shouldNot typeCheck
 
     // Multiple `.nVar` candidates:
-    """ p"${n.nVar} ${k.nVar}" """ shouldNot compile
-    """ p"${n.nVar} ${n.nVar}" """ shouldNot compile
+    """ p"${n.nVar} ${k.nVar}" """ shouldNot typeCheck
+    """ p"${n.nVar} ${n.nVar}" """ shouldNot typeCheck
 
     // No `nVar`s:
-    """ p"Simple string" """ shouldNot compile
-    """ p"$s str" """ shouldNot compile
-    """ p"${S.s}${S.es}" """ shouldNot compile
+    """ p"Simple string" """ shouldNot typeCheck
+    """ p"$s str" """ shouldNot typeCheck
+    """ p"${S.s}${S.es}" """ shouldNot typeCheck
 
     // Many many many suffixes:
     p"${S.s}${S.s}${S.es}${S.es}${S.s}${S.es}$n" shouldBe "{p:10:ssesesses10:10}"
@@ -170,8 +170,8 @@ class MacroTest extends FlatSpec with Matchers {
 
     // `a &> b` operator:
     p"There ${"is" &> "are"} $n pen${S.s}" shouldBe "{p:There is 10 pen:There are 10 pens:10}"
-    """ p"${p &> "1"}" """ shouldNot compile
-    """ p"${"1" &> p}" """ shouldNot compile
+    """ p"${p &> "1"}" """ shouldNot typeCheck
+    """ p"${"1" &> p}" """ shouldNot typeCheck
 
     p"${S.s}${"" &> ""}${"x" &> ""}${"" &> "y"}${S.es}${"z" &> "w"}$n" shouldBe "{p:xz10:syesw10:10}"
   }
